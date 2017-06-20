44f81056fe4a2cdad0240cfb9f72e0562d6dde23
Zachary Bergmann <zbbergma@gmail.com>
2017-06-20 16:01:16 -0500

    (prog) Comment out webpack build, add bower components for FE deps

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,24 @@
-var path = require('path');
+// const path = require('path');
+// const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-module.exports = {
-  entry: [
-    './node_modules/angular/angular.min.js',
-    './src/public/app/app.js'
-  ],
-  output: {
-    path: path.join(__dirname + '/src/public/dist'),
-    filename: 'bundle.js'
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['es2015']
-        }
-      },
-      {
-        test: /\.css$/,
-        exclude: /node_modules/,
-        loader: 'style-loader!css-loader'
-      }
-    ]
-  }
-};
+// module.exports = {
+//   entry: './src/public/app/app.js',
+//   output: {
+//     path: path.resolve(__dirname, 'src/public/dist'),
+//     filename: 'bundle.js',
+//   },
+//   module: {
+//     rules: [
+//       {
+//         test: /\.scss$/,
+//         use: ExtractTextPlugin.extract({
+//           fallback: 'style-loader',
+//           use: ['css-loader', 'sass-loader'],
+//         }),
+//       },
+//     ],
+//   },
+//   plugins: [
+//     new ExtractTextPlugin('style.css'),
+//   ],
+// };
