6a02e5010976b76aef5610b756f09a227d643f03
Zachary Bergmann <zbbergma@gmail.com>
2017-06-20 17:18:53 -0500

    (prog) Fix binding of data for data viz, start formatting

--- src/public/app/dataviz/dataviz.component.js
+++ src/public/app/dataviz/dataviz.component.js
@@ -2,8 +2,13 @@ angular
   .module('dataviz')
   .component('dataviz', {
     templateUrl: 'app/dataviz/dataviz.template.html',
-    controller: ['$rootScope', '$location', function DataVizController($rootScope, $location) {
-      // this.data = [[1, 3], [4, 4]];
+    controller: function DataVizController() {
+      this.items = [];
+
+      this.addItem = (dataPt) => {
+        this.items.push(dataPt);
+        console.log(this.items, 'new item list');
+      };
 
       this.options = {
         chart: {
@@ -38,30 +43,30 @@ angular
         },
       };
 
-      this.data = generateData(4, 40);
-      console.log(this.data, 'this is your data generated');
-      /* Random Data Generator (took from nvd3.org) */
-      function generateData(groups, points) {
-        const data = [];
-        const shapes = ['circle'];
-        const random = d3.random.normal();
+      // this.data = generateData(4, 40);
+      // console.log(this.data, 'this is your data generated');
+      // /* Random Data Generator (took from nvd3.org) */
+      // function generateData(groups, points) {
+      //   const data = [];
+      //   const shapes = ['circle'];
+      //   const random = d3.random.normal();
 
-        for (let i = 0; i < groups; i++) {
-          data.push({
-            key: 'Group ' + i,
-            values: [],
-          });
+      //   for (let i = 0; i < groups; i++) {
+      //     data.push({
+      //       key: 'Group ' + i,
+      //       values: [],
+      //     });
 
-          for (let j = 0; j < points; j++) {
-            data[i].values.push({
-              x: random(),
-              y: random(),
-              size: Math.random(),
-              shape: shapes[j % 6],
-            });
-          }
-        }
-        return data;
-      }
-    }],
+      //     for (let j = 0; j < points; j++) {
+      //       data[i].values.push({
+      //         x: random(),
+      //         y: random(),
+      //         size: Math.random(),
+      //         shape: shapes[j % 6],
+      //       });
+      //     }
+      //   }
+      //   return data;
+      // }
+    },
   });
