2bcd9bcfa3aab01e416adaa9755397a30f392fcd
Zachary Bergmann <zbbergma@gmail.com>
2017-06-21 15:30:24 -0500

    (prog) Refactor chart generation to directive

--- src/public/app/dataviz/dataviz.module.js
+++ src/public/app/dataviz/dataviz.module.js
@@ -1,3 +1,3 @@
 angular.module('dataviz', [
-  'tc.chartjs',
+  'chartGenDirective',
 ]);
---
+++ src/public/app/directives/chartgen.directive.js
@@ -0,0 +1,85 @@
+angular.module('chartGenDirective')
+  .controller('chartGenController', ($scope) => {
+    $scope.curMinX = -5;
+    $scope.curMinY = -5;
+    $scope.curMaxX = 5;
+    $scope.curMaxY = 5;
+
+    $scope.chartData = {
+      datasets: [
+        {
+          label: 'Dataset #1',
+          data: [],
+          backgroundColor: '#FF6384',
+          hoverBackgroundColor: '#FF6384',
+        },
+      ],
+    };
+
+    $scope.chartOptions = {
+      scales: {
+        yAxes: [
+          {
+            id: 'A',
+            type: 'linear',
+            position: 'left',
+            scaleLabel: {
+              display: true,
+              labelString: 'Y Position',
+            },
+            ticks: {
+              max: $scope.curMaxY,
+              min: $scope.curMinY,
+            },
+          }],
+        xAxes: [
+          {
+            id: 'B',
+            type: 'linear',
+            position: 'bottom',
+            scaleLabel: {
+              display: true,
+              labelString: 'X Position',
+            },
+            ticks: {
+              max: $scope.curMaxX,
+              min: $scope.curMinX,
+            },
+          }],
+      },
+    };
+
+    $scope.onChartClick = event => console.log(event);
+
+    $scope.createPlot = (dataPtsStr) => {
+      const regex = /\)[^0-9]+\(*/;
+      const applyRegex = new RegExp(regex, 'gi');
+      const cleanedDataPts = dataPtsStr.trim().slice(1, dataPtsStr.length - 1).split(applyRegex);
+      cleanedDataPts.forEach((xyPtsStr) => {
+        const xyTuple = xyPtsStr.split(',');
+
+        $scope.curMinX = $scope.curMinX === null || +xyTuple[0] < $scope.curMinX ? +xyTuple[0] : $scope.curMinX;
+        $scope.chartOptions.scales.xAxes[0].ticks.min = $scope.curMinX;
+        $scope.curMinY = $scope.curMinY === null || +xyTuple[1] < $scope.curMinY ? +xyTuple[1] : $scope.curMinY;
+        $scope.chartOptions.scales.yAxes[0].ticks.min = $scope.curMinY;
+        $scope.curMaxX = $scope.curMaxX === null || +xyTuple[0] > $scope.curMaxX ? +xyTuple[0] : $scope.curMaxX;
+        $scope.chartOptions.scales.xAxes[0].ticks.max = $scope.curMaxX;
+        $scope.curMaxY = $scope.curMaxY === null || +xyTuple[1] > $scope.curMaxY ? +xyTuple[1] : $scope.curMaxY;
+        $scope.chartOptions.scales.yAxes[0].ticks.max = $scope.curMaxY;
+
+        $scope.chartData.datasets[0].data.push({
+          x: +xyTuple[0],
+          y: +xyTuple[1],
+          r: 10,
+        });
+      });
+    };
+  })
+  .directive('chartgen', () => {
+    return {
+      controller: 'chartGenController',
+      controllerAs: 'ctrl',
+      bindToController: true,
+      templateUrl: 'app/directives/chartgen.template.html',
+    };
+  });
